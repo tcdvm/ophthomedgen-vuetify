c0d1a66427ba6132dbd4ae623598d5680eccdf2d
TChen <tchendvm@gmail.com>
2019-01-17 00:24:22 -0500

    Changed title font

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Ophtho Drugs</span>
-        <!-- <span class="font-weight-light">MATERIAL DESIGN</span> -->
+        <span>Ophtho</span>
+        <span class="font-weight-light">Drugs</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-dialog v-model="instructions" fullwidth>
